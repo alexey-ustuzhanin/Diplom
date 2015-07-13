6009f0414b27b90bd8d220c772600a5b1e39da39
Alexey <alexey.ustuzhanin@gmail.com>
2015-07-13 15:35:54 +0300

    //

--- src/Diplom.java
+++ src/Diplom.java
@@ -1,5 +1,5 @@
 public class Diplom {
-    public static void main(String[] args) {
+    public static void main(String[] args) {//
         MergeSort mergeSort = new MergeSort();
         WorkWithFile.writeFile(mergeSort.sort(WorkWithFile.getArrayFromFile()));
         System.out.println("Look at the file.");
